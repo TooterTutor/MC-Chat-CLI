5e2aba3b013d719185dcc4913131ee768ffcf16d
TooterTutor <62804230+TooterTutor@users.noreply.github.com>
2023-07-19 00:15:57 -0600

    Updated index.js
    Added logging

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const mineflayer = require('mineflayer')
 const fs = require('node:fs')
 
+const output = fs.createWriteStream('./logs/chatlog.log')
+const myConsole = new console.Console(output)
+
+require('console-stamp')(myConsole, {
+    stdout: output,
+    format: ':date(HH:MM:ss)'
+})
+
 var defaultRegEx = new RegExp("[^>]+")
 
-if (process.argv.length > 6) {
-    console.log('Usage : node example.js [<host>] [<port>] [<name>] [<password>]')
+if (process.argv.length > 4) {
+    console.log('Usage : node example.js [<host>] [<port>]')
     process.exit(1)
 }
 
@@ -17,7 +25,7 @@ const bot = mineflayer.createBot({
     username: mcAuth.login,
     password: mcAuth.password,
     auth: "microsoft",
-    version: '1.20.1',
+    version: '1.20',
     hideErrors: false
 })
 
@@ -29,3 +37,8 @@ bot.once('inject_allowed', () => {
 bot.loadPlugin(require('mineflayer-dashboard')({
     chatPattern: defaultRegEx
 }))
+
+
+bot.on("chat", (username, message) => {
+    myConsole.log(`[${username}] ${message}`);
+})
